a5f602a6c23c63764a15decf04f4b9cde874b849
Opolskiy <opolskiy@bpcbt.com>
2019-05-13 16:44:49 +0300

    Added timer and total time

--- src/main/scala/com/training/TodoApp.scala
+++ src/main/scala/com/training/TodoApp.scala
@@ -1,38 +1,67 @@
 package com.training
 
-import slinky.core.{Component, StatelessComponent, SyntheticEvent}  //nodisplay
-import slinky.core.annotations.react //nodisplay
-import slinky.web.html._ //nodisplay
-import org.scalajs.dom.{html, Event} //nodisplay
+import slinky.core.{Component, StatelessComponent, SyntheticEvent}
+import slinky.web.html.{div, _}
+import org.scalajs.dom.{Event, html}
 
-import scala.scalajs.js.Date //nodisplay
+import scala.scalajs.js.Date
 
-case class TodoItem(text: String, id: Long)
+import slinky.core.annotations.react
+import slinky.core.Component
+import slinky.web.html._
+import org.scalajs.dom.window._
+
+case class IntervalItem(id: Long, paidTime: Long, dontStopPayingTime: Long, dontPayTime: Long, description: String)
 
 @react class TodoApp extends Component {
   type Props = Unit
-  case class State(items: Seq[TodoItem], text: String)
+  case class State(items: Seq[IntervalItem], description: String, paidTime: Long, seconds: Long)
 
-  override def initialState = State(Seq.empty, "")
+  def tick(): Unit = setState(prevState => prevState.copy(seconds = prevState.seconds + 1))
+
+  private var interval = -100
+
+  override def componentDidMount(): Unit = {
+    interval = setInterval(() => tick(), 1000)
+  }
+
+  override def componentWillUnmount(): Unit = {
+    clearInterval(interval)
+  }
+
+  override def initialState = State(Seq.empty, "", 0, 0)
 
   def handleChange(e: SyntheticEvent[html.Input, Event]): Unit = {
     val eventValue = e.target.value
-    setState(_.copy(text = eventValue))
+    setState(_.copy(description = eventValue))
+  }
+
+  def handleChange2(e: SyntheticEvent[html.Input, Event]): Unit = {
+    val eventValue = e.target.value
+    setState(_.copy(paidTime = eventValue.toLong))
   }
 
   def handleSubmit(e: SyntheticEvent[html.Form, Event]): Unit = {
     e.preventDefault()
 
-    if (state.text.nonEmpty) {
-      val newItem = TodoItem(
-        text = state.text,
-        id = Date.now().toLong
+    clearInterval(interval)
+    interval = setInterval(() => tick(), 1000)
+
+    if (state.description.nonEmpty) {
+      val newItem = IntervalItem(
+        id = Date.now().toLong,
+        description = state.description,
+        paidTime = state.paidTime,
+        dontStopPayingTime = 0,
+        dontPayTime =0
       )
 
       setState(prevState => {
         State(
           items = prevState.items :+ newItem,
-          text = ""
+          description = "",
+          paidTime = 0,
+          seconds = 0
         )
       })
     }
@@ -40,30 +69,35 @@ case class TodoItem(text: String, id: Long)
 
   override def render() = {
     div(
-      h3("TODO"),
-      TodoList(items = state.items),
+      h3("Iron Work Man"),
+      h3("Seconds: ", state.seconds.toString),
       form(onSubmit := (handleSubmit(_)))(
         input(
           onChange := (handleChange(_)),
-          value := state.text
+          value := state.description
+        ),
+        input(
+          onChange := (handleChange2(_)),
+          value := state.paidTime.toString
         ),
         button(s"Add #${state.items.size + 1}")
-      )
+      ),
+      TodoList(items = state.items)
     )
   }
 }
 
 @react class TodoList extends StatelessComponent {
-  case class Props(items: Seq[TodoItem])
+  case class Props(items: Seq[IntervalItem])
 
   override def render() = {
-    ul(
-      props.items.map { item =>
-        li(key := item.id.toString)(item.text)
-      }
+    div(
+      ul(
+        props.items.map { item =>
+          li(key := item.id.toString)(s"Paid time: ${item.paidTime}, Description: ${item.description}")
+        }
+      ),
+      div(s"Total Paid time = ${props.items.foldLeft(0L)((acc, item) => acc + item.paidTime)}")
     )
   }
 }
-
-//display:ReactDOM.render(TodoApp(), mountNode)
-//run:TodoApp() //nodisplay
